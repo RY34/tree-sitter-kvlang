5f13fb2ce483301dbf4674cab39d73ed29af9f75
RY34 <klaudek1ryba@gmail.com>
2025-06-25 19:57:01 +0200

    Update highlights.scm

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,8 +1,7 @@
-(string) @string
 "canvas" @keyword
 "\\B< *-*([A-Z][a-zA-Z0-9_]*)\\b>" @constructor
 "\\B\\s([A-Z][a-zA-Z0-9_]*):[\\s]*\\s" @keyword
 (pair key: (string) @variable)
 (number) @number
-["@", ",", ":"] @string.special.symbol
+["@", ",", ":"] @keyword
 "[\\[] *(([A-Z][a-zA-Z0-9_]*) *@ *)(([a-zA-Z_][a-zA-Z0-9_]*) *,* *)(([a-zA-Z_][a-zA-Z0-9_]*) *,* *)(([a-zA-Z_][a-zA-Z0-9_]*) *,* *)(([a-zA-Z_][a-zA-Z0-9_]*) *,* *)(([a-zA-Z_][a-zA-Z0-9_]*) *[\\]]:?)" @keyword
